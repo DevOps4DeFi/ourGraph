4f66fc64d758c93e76f3715564963b3fa4e81f39
BenLeibig <ben.leibig@gmail.com>
2021-09-19 01:38:01 +0200

    update rds version

--- rds.tf
+++ rds.tf
@@ -38,7 +38,7 @@ module "rds" {
 
   # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
   engine               = "postgres"
-  engine_version = "13.1"
+  engine_version = "13.3"
   family               = "postgres13" # DB parameter group
   major_engine_version = "13"         # DB option group
   instance_class       = var.rds_instance_type
